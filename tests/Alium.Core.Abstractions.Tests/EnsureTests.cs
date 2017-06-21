542925a373a030fba10deb98caa945caefdbbece
Matthew Abbott <matthew.abbott@outlook.com>
2017-06-21 18:11:55 +0100

    Added tests for the Ensure type

---
+++ tests/Alium.Core.Abstractions.Tests/EnsureTests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Alium FX. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Alium
+{
+    using System;
+    using Xunit;
+
+    /// <summary>
+    /// Provides tests for the <see cref="Ensure"/> type.
+    /// </summary>
+    public class EnsureTests
+    {
+        [Fact]
+        public void IsNotNullOrEmpty_ThrowsExceptionForNullOrEmptyValues()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => Ensure.IsNotNullOrEmpty((string) null, ""));
+            Assert.Throws<ArgumentException>(() => Ensure.IsNotNullOrEmpty(string.Empty, ""));
+        }
+
+        [Fact]
+        public void IsNotNullOrEmpty_ReturnsValue()
+        {
+            // Arrange
+            string value = "hello";
+
+            // Act
+            string value2 = Ensure.IsNotNullOrEmpty(value, "");
+
+            // Assert
+            Assert.Equal(value, value2);
+        }
+    }
+}
